9ac2a886d981b2e0635620dc3b38db3490356c86
Maria Seljak <lseljak92@gmail.com>
2018-08-21 07:18:37 -0800

    Modify Enemy prototype Update function

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    //Upload enemy's image
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -13,7 +13,20 @@ var Enemy = function() {
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
-    // all computers.
+    // all computers. 
+    this.x += this.speed * dt;
+    if(this.x > 550){
+        this.x = -100;
+        this.speed = 100 + Math.floor(Math.random() * 512);
+    }   
+
+    if(player.x < this.x +60 &&
+        player.x + 37 > this.x &&
+        player.y < this.y + 25 &&
+        30 + player.y > this.y) {
+            player.x = 200;
+            player.y = 380;
+        }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,6 +37,7 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
 
 
 // Now instantiate your objects.
